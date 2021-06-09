fdee0a4d6c04d91e7983f2d9c6ee554101ac18dd
Greg <Gdixon5125@gmail.com>
2021-06-09 12:31:26 -0400

    week 4 homework

---
+++ app1.js
@@ -0,0 +1,24 @@
+console.log("Hello world!");
+alert("This is a warning");
+/*4) Our index.html should have the boilerplate and a single with the content of "Check the console..."
+5) Make sure to link your index.html to app1.js, from now on out we will be working out of app1.js file.
+6) Write a message to the console that says "Hello world!"
+7) Write a warning to the console that says "This is a warning!"
+8) Write an error to the console that says "Error! System critical!"
+9) Declare a variable named var1
+//10) Assign a string data type value to var1
+//11) Write the value of var1 to the console
+//12) Write a blank line to the console. (I showed one way to do this in class, there are some others feel free to use those instead if you'd like, practice your google-fu)
+//13) Define and assign a variable named var2, the value should be of the number type
+//14) Evaluate var1 + var2 and write the result to the console (You can store the result in a variable and write it to the console, or pass the expression into a method that will remain un-named)
+//15) Could you guess the result? Write your guess in an inline comment next to where you're writing it to the console
+//16) Add in a multi line comment and write some of the types we covered in class today on individual lines
+//17) Write the type of the var1 variable to the console
+//18) Can you guess the result? Write your guess in an inline comment next to where you're writing to the console.
+//19) Declare a variable named var3 and assign it the string value text
+//20) Write the value of var3 to the console
+//20) Reassign var3 to a boolean value
+//21) Write the value of var3 to the console
+//22) Reassign var1 to the number 5
+//23) Reassign var2 to the string 5
+//24) In any order or in however many lines you'd like, parse var2 from a string to a number and add it to var1, write your result to the console.
