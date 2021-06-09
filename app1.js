eff8027a0170aa472efe2008e2836e27f9d640fd
Greg <Gdixon5125@gmail.com>
2021-06-09 16:38:22 -0400

    new

--- app1.js
+++ app1.js
@@ -1,28 +1,48 @@
 console.log("Hello world!");
 console.warn("This is a warning");
 console.error("Error! System critical!");
-var 'kids' = var1;
-console.info(" ");
-String = var1 + "\n" + "4 parents";
-var var2 = "6";
-var var1 = var2 + var1
 
+
+//9) Declare a variable named var1
+var var1;
+//10) Assign a string data type value to var1
+
+var1 = "parents" + " " + "3 kids";n
+//11) Write the value of var1 to the console
+console.log(var1);
+//12) Write a blank line to the console. (I showed one way to do this in class, there are some others feel free to use those instead if you'd like, practice your google-fu)
+console.info("  ")
+//13) Define and assign a variable named var2, the value should be of the number type
+var var2 = 2 ;
+//14) Evaluate var1 + var2 and write the result to the console (You can store the result in a variable and write it to the console, or pass the expression into a method that will remain un-named)
+var var3 = var2 + " " + var1 
+
+//15) Could you guess the result? Write your guess in an inline comment next to where you're writing it to the console
+
+//2parents 3 kids 
+
+//16) Add in a multi line comment and write some of the types we covered in class today on individual lines
 
 /*
-9) Declare a variable named var1
-//10) Assign a string data type value to var1
-//11) Write the value of var1 to the console
-//12) Write a blank line to the console. (I showed one way to do this in class, there are some others feel free to use those instead if you'd like, practice your google-fu)
-//13) Define and assign a variable named var2, the value should be of the number type
-//14) Evaluate var1 + var2 and write the result to the console (You can store the result in a variable and write it to the console, or pass the expression into a method that will remain un-named)
-//15) Could you guess the result? Write your guess in an inline comment next to where you're writing it to the console
-//16) Add in a multi line comment and write some of the types we covered in class today on individual lines
+ you can put the <script src="/app1.js"></script> in the head & body.
+ To add a string to a var just doing var var1 = "anything" + "numbers" 
+*/
+
 //17) Write the type of the var1 variable to the console
+
 //18) Can you guess the result? Write your guess in an inline comment next to where you're writing to the console.
+
 //19) Declare a variable named var3 and assign it the string value text
+var var3 = var2 + " " + var1 
 //20) Write the value of var3 to the console
+
 //20) Reassign var3 to a boolean value
+
 //21) Write the value of var3 to the console
+
 //22) Reassign var1 to the number 5
+
 //23) Reassign var2 to the string 5
+
 //24) In any order or in however many lines you'd like, parse var2 from a string to a number and add it to var1, write your result to the console.
+
