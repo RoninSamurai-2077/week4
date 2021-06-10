32d2a4b0748b040ec57d31e6e08206aa6f9e6d2e
Greg <Gdixon5125@gmail.com>
2021-06-09 20:44:43 -0400

    done

--- app1.js
+++ app1.js
@@ -18,34 +18,28 @@ console.info(var1 + var2 = parents 3 kids2);
 //2parents 3 kids 
 
 //16) Add in a multi line comment and write some of the types we covered in class today on individual lines
-
 /*
  you can put the <script src="/app1.js"></script> in the head & body.
  To add a string to a var just doing var var1 = "anything" + "numbers" 
 */
-
 //17) Write the type of the var1 variable to the console
-console.info (var1);
-//18) Can you guess the result? Write your guess in an inline comment next to where you're writing to the console.
 
+//18) Can you guess the result? Write your guess in an inline comment next to where you're writing to the console.
+console.info (typeof var1); //string
 //19) Declare a variable named var3 and assign it the string value text
 var var3 = var2 + " " + "and"+ var1;
 //20) Write the value of var3 to the console
-console.info
-//20) Reassign var3 to a boolean value
-boolean var3 = true;
-
-if ( !var3 ) {
-System.out.println("it's flase");
-}
-else {
-System.out.println("it's true");
-}
-//21) Write the value of var3 to the console
 console.info(var3);
-//22) Reassign var1 to the number 5
-var 5 = var1;
-//23) Reassign var2 to the string 5
-var string 5 = var2;
-//24) In any order or in however many lines you'd like, parse var2 from a string to a number and add it to var1, write your result to the console.
+//20) Reassign var3 to a boolean value
+ var3 = true;
 
+//21) Write the value of var3 to the console
+console.log(var3);
+//22) Reassign var1 to the number 5
+var1 = 5;
+//23) Reassign var2 to the string 5 
+var2 = "5";
+//24) In any order or in however many lines you'd like, parse var2 from a string to a number and add it to var1, write your result to the console.
+console.log(
+    parseInt(var2) + var1
+);
